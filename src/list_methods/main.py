1745c5f1b65481022243042c1a5bcef583b12ae1
Theodor René Carlsen <theodor.rene@gmail.com>
2021-12-01 18:31:26 +0100

    add new performance feature

--- src/list_methods/main.py
+++ src/list_methods/main.py
@@ -1,19 +1,28 @@
 from functools import reduce
+from warnings import warn
 
 class L:
     """
     Tired of not having map, filter and reduce as easily available like in
     Javascript, Java, Kotlin etc?  Fear not this is the class for you.
     """
-    def __init__(self,l):
+    def __init__(self,l,mutable=False):
+        """
+        :init_l: inital list, useful when completing an operation and resetting the state
+        :curr_l: current list
+        :mutable: determines if a copy is needed, this removes the opportunity
+        to reset the state of the object, but reduces memory overhead of using
+        .copy()
+        """
         self.init_l=l
-        self.curr_l=l.copy()
+        self.curr_l=l.copy() if not mutable else l
+        self.mutable=mutable
 
     def __str__(self):
         """
         String representation of the list
         Note this has a effect on performance/space,since it consumes the
-        iterator and instansiates the list
+        iterator and instantiates the list
         """
         self.curr_l=list(self.curr_l)
         return f"L({str(self.curr_l)}"
@@ -43,7 +52,7 @@ class L:
         when reduce is called and collect
         """
         final_result = self.curr_l # Save the current list into a variable
-        self.curr_l=self.init_l.copy() # set curr_l back to the inital list that the class was instansiated with
+        self.curr_l=self.init_l.copy() # set curr_l back to the inital list that the class was instantiated with
         return final_result
 
     def c(self):
@@ -51,6 +60,9 @@ class L:
         Collect
         Collecting the final result into a list and reseting the object.
         """
+        if self.mutable:
+            warn("This List is mutable and therefore does not need to be instantiated into a list",SyntaxWarning)
+            return self.curr_l
         final_list = self._reset()
         return list(final_list)
 
